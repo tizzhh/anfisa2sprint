148249a1735f39c48dbba4dcdedfcf8758e3f893
Vadim Kuznetsov <darovadraste@gmail.com>
2023-11-06 21:10:39 +0300

    filter, exclude, conditionals using Q, order, get, get_object_or_404.

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
+from django.db.models import Q
 from ice_cream.models import IceCream
 
 
 def index(request):
     template = 'homepage/index.html'
-    # Запишите в переменную ice_cream_list новый QuerySet
-    ice_cream_list = IceCream.objects.values('title', 'id', 'description')
+    ice_cream_list = IceCream.objects.values('title', 'id', 'description').filter(is_on_main=True, is_published=True).order_by('title')[1:4]
+    # ice_cream_list = IceCream.objects.values('title', 'id', 'description').filter(
+    #     Q(is_published=True) & (Q(is_on_main=True) | Q(title__contains='пломбир'))
+    # )
     context = {
         'ice_cream_list': ice_cream_list,
     }
